912801e16ae69fdab8087b80ebdeb962c7eed9b3
Roming22 <roming22@gmail.com>
2023-10-10 21:14:51 -0400

    use busy_wait_us()
    Pulse detected between 9 and 31 microseconds. Expected 30us.

--- mymk/Pixels.hpp
+++ mymk/Pixels.hpp
@@ -3,28 +3,28 @@
 
 #include <Adafruit_NeoPixel.h>
 
-class Pixels: public Adafruit_NeoPixel {
-    private:
-    // Singleton
-    Pixels(const int pin, const int count) : Adafruit_NeoPixel(count, pin) {}
+class Pixels : public Adafruit_NeoPixel {
+private:
+  // Singleton
+  Pixels(const int pin, const int count) : Adafruit_NeoPixel(count, pin) {}
 
-    public:
-    // Singleton
-    Pixels(const Pixels& obj) = delete;
-    Pixels& operator=(const Pixels& obj) = delete;
-    static Pixels* get(const int pin = 0, const int count = 0) {
-        static Pixels* instance = new Pixels(pin, count);
-        return instance;
-    }
-    
-    static void initialize(const int pin, const int count) {
-        Pixels* _pixels = get(pin, count);
-        _pixels->begin();
-        _pixels->clear();
-        _pixels->show();
-        _pixels->setBrightness(255);
-        Serial.println("[Pixels] OFF");
-    }
+public:
+  // Singleton
+  Pixels(const Pixels &obj) = delete;
+  Pixels &operator=(const Pixels &obj) = delete;
+  static Pixels *get(const int pin = 0, const int count = 0) {
+    static Pixels *instance = new Pixels(pin, count);
+    return instance;
+  }
+
+  static void initialize(const int pin, const int count) {
+    Pixels *_pixels = get(pin, count);
+    _pixels->begin();
+    _pixels->clear();
+    _pixels->show();
+    _pixels->setBrightness(255);
+    Serial.println("[Pixels] OFF");
+  }
 };
 
 #endif
